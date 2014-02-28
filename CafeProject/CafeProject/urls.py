2996f74de7fa90661535c0d715b3801885037d7a
Vipul Sarin <vipul.sarin@gmail.com>
2014-02-28 17:42:15 +0530

    Create BLRCafe project structure

---
+++ CafeProject/CafeProject/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'CafeProject.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+     url(r'^Cafe/', include('Cafe.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
