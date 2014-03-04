876e97cf42100af706cf54c01c16de96fb8f60dd
Vipul Sarin <vipul.sarin@ugamsolutions.com>
2014-03-04 11:43:42 +0530

    add database models and form validation

--- CafeProject/Cafe/models.py
+++ CafeProject/Cafe/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from uuidfield import UUIDField
 # Create your models here.
 class User(models.Model):
     empid = models.IntegerField(primary_key=True)
@@ -8,5 +8,18 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    inventory = models.IntegerField(max_length=5)
+    price = models.IntegerField(max_length=10)
+    
+class Transaction(models.Model):
+    orderid = UUIDField(auto=True)
+    empid = models.ForeignKey(User)
+    productid = models.ForeignKey(Product)
+    time = models.DateTimeField()
+    quantity = models.IntegerField(max_length=5)
+    
     
     
