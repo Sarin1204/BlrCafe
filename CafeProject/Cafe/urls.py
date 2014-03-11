0747ee8fe1740dce1664d65c1eed0f3617ca5edc
Vipul Sarin <vipul.sarin@ugamsolutions.com>
2014-03-11 10:00:56 +0530

    Added logout functionality

--- CafeProject/Cafe/urls.py
+++ CafeProject/Cafe/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from Cafe import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'index', views.index, name='index'),
     url(r'menu', views.menu, name='menu'),
     url(r'signup', views.index, name='index'),
     url(r'signin', views.signin, name='signin'),
